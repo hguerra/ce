bb16cf35bb5937751d56c72143022ce3ac6f9ba3
Heitor Carneiro <heitorguerrac@gmail.com>
2017-02-02 12:28:30 -0200

    #3. Adding UsuarioController.

--- crud/src/main/java/org/carneiro/ce/controller/UsuarioController.java
+++ crud/src/main/java/org/carneiro/ce/controller/UsuarioController.java
@@ -1,22 +1,53 @@
 package org.carneiro.ce.controller;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.carneiro.ce.model.impl.Saque;
+import org.carneiro.ce.model.impl.Usuario;
+import org.carneiro.ce.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
+import javax.validation.Valid;
+import java.util.Collection;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 /**
  * @author heitor
  * @since 28/01/17
  */
-@SpringBootApplication
-@Controller
+@RestController
+@RequestMapping("/usuario")
 public class UsuarioController {
-	@RequestMapping("/")
+	@Autowired
+	private UsuarioService usuarioService;
+
+	public void setUsuarioService(UsuarioService usuarioService) {
+		this.usuarioService = usuarioService;
+	}
+
+	@RequestMapping(value = {"/", ""}, method = POST, produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
 	@ResponseBody
-	public String index(){
-		return "Hora: " + LocalDateTime.now();
+	public Usuario cadastrarUsuario(@Valid @RequestBody Usuario usuario) {
+		return this.usuarioService.cadastrar(usuario);
+	}
+
+	@RequestMapping(value = {"/", ""}, method = GET, produces = APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public Collection<Usuario> listarUsuarios() {
+		return this.usuarioService.listarUsuarios();
+	}
+
+	@RequestMapping(value = {"/{nome}"}, method = GET, produces = APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public Usuario buscarUsuario(@PathVariable String nome) {
+		return this.usuarioService.buscar(nome);
+	}
+
+	@RequestMapping(value = {"/{nome}/saque"}, method = GET, produces = APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public Collection<Saque> listarSaques(@PathVariable String nome) {
+		return this.usuarioService.listarSaques(nome);
 	}
 }
