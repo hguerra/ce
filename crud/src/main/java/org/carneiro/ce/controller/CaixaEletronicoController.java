b131193458bbfd8d56e4eeff20dfb282f414df15
Heitor Carneiro <heitorguerrac@gmail.com>
2017-02-02 12:38:18 -0200

    #3. Adding CaixaEletronico implementation.

--- crud/src/main/java/org/carneiro/ce/controller/CaixaEletronicoController.java
+++ crud/src/main/java/org/carneiro/ce/controller/CaixaEletronicoController.java
@@ -1,8 +1,53 @@
 package org.carneiro.ce.controller;
 
+import org.carneiro.ce.model.impl.CaixaEletronico;
+import org.carneiro.ce.model.impl.Saque;
+import org.carneiro.ce.service.CaixaEletronicoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Collection;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
 /**
  * @author heitor
  * @since 02/02/17
  */
+@RestController
+@RequestMapping("/caixa")
 public class CaixaEletronicoController {
+	@Autowired
+	private CaixaEletronicoService caixaEletronicoService;
+
+	public void setCaixaEletronicoService(CaixaEletronicoService caixaEletronicoService) {
+		this.caixaEletronicoService = caixaEletronicoService;
+	}
+
+	@RequestMapping(value = {"/", ""}, method = POST, produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public CaixaEletronico cadastrarUsuario(@Valid @RequestBody CaixaEletronico caixa) {
+		return this.caixaEletronicoService.cadastrar(caixa);
+	}
+
+	@RequestMapping(value = {"/", ""}, method = GET, produces = APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public Collection<CaixaEletronico> listarCaixasEletronicos() {
+		return this.caixaEletronicoService.listarCaixasEletronicos();
+	}
+
+	@RequestMapping(value = {"/{nome}"}, method = GET, produces = APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public CaixaEletronico buscarCaixaEletronico(@PathVariable String nome) {
+		return this.caixaEletronicoService.buscar(nome);
+	}
+
+	@RequestMapping(value = {"/{nome}/saque"}, method = GET, produces = APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public Collection<Saque> listarSaques(@PathVariable String nome) {
+		return this.caixaEletronicoService.listarSaques(nome);
+	}
 }
